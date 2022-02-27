e2b15b2ff7b1ffb0601b8372c5af4e8288bf5252
Stefan Moravcik <moravcik@virsas.com>
2022-02-27 02:43:22 +0100

    added some values to default variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
-variable "region" {}
-variable "organization" {}
-variable "description" {}
+variable "region" { default = "eu-west-1" }
+variable "organization" { default = "123456789012"}
+variable "description" { default = "KMS key" }
